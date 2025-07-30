ae46d9ea6fee1fcd475612c34d28742d47082c1a
Clemomsanii <clemomsanii@gmail.com>
2025-07-30 11:34:05 +0300

    Create sites.py

---
+++ sites.py
@@ -0,0 +1,17 @@
+# sites.py
+
+job_sites = [
+    {
+        "name": "BrighterMonday",
+        "url": "https://www.brightermonday.co.ke/jobs"
+    },
+    {
+        "name": "Safaricom Careers",
+        "url": "https://www.safaricom.co.ke/about/careers"
+    },
+    {
+        "name": "Equity Bank Careers",
+        "url": "https://equitygroupholdings.com/careers"
+    },
+    # Add more manually here
+]
