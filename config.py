2c95b03033de241a34ac4a108e69b7f069c66239
Clemomsanii <clemomsanii@gmail.com>
2025-07-30 11:58:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-EMAIL_SENDER = "your_email@gmail.com"
-EMAIL_PASSWORD = "your_app_password"
-EMAIL_RECEIVER = "recipient@example.com"
+EMAIL_SENDER = "clemomsanii@gmail.com"
+EMAIL_PASSWORD = "Clement25330129"
+EMAIL_RECEIVER = "clemomsanii@gmail.com"
